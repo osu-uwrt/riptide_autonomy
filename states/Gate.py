7225c0452edfd922b0b32e2afe91ac0a938b4170
nick <iten.pn@gmail.com>
2020-01-22 20:22:49 -0500

    Added more movement optinos

---
+++ states/Gate.py
@@ -0,0 +1,19 @@
+import rospy
+import smach
+from Move import *
+
+def main():
+    rospy.init_node('movement_test')
+    sm = smach.StateMachine(outcomes=['done', 'fail'])
+    sm.userdata.args = {'x' : 1, 'y' : 2}
+    sm.type = 'move'
+
+    with sm:
+        smach.StateMachine.add('MOVE', Move(),
+                                transitions={'Success': 'done',
+                                            'Failure': 'fail'},
+                                remapping={'type','type','args':'args'})
+    outcome = sm.execute()
+
+if __name__ == '__main__':
+    main()
--- states/Move.py
+++ states/Move.py
@@ -6,10 +6,16 @@ class Move(smach.State):
     def __init__(self):
         smach.State.__init__(self, 
             outcomes=['Success', 'Failure'],
-            input_keys=['x', 'y'])
+            input_keys=['type','args'])
     
     def execute(self, userdata):
-        rospy.loginfo('Moving by vector <%f, %f>'%(userdata.x, userdata.y))
-        moveAction(userdata.x, userdata.y).wait_for_result()
-        rospy.loginfo('I moved uwu')
+        if type == 'move':
+            rospy.loginfo('Moving by vector <%f, %f>'%(userdata.args.x, userdata.args.y))
+            moveAction(userdata.args.x, userdata.args.x).wait_for_result()
+        elif type == 'depth':
+            rospy.loginfo('Moving down %f'%userdata.args.depth)
+            depthAction(userdata.args.depth)
+        elif type == 'yaw':
+            rospy.loginfo('Moving with angle %f'%userdata.args.angle)
+            yawAction(userdata.args.angle)
         return 'Success'
