f70f32028dbe67f6a8bc077826ff15dfa15898ed
nick <iten.pn@gmail.com>
2020-01-26 11:40:36 -0500

    Fine tuning

--- states/Gate.py
+++ states/Gate.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python
+
 import rospy
 import smach
 from Move import *
@@ -5,10 +7,29 @@ from Move import *
 def main():
     rospy.init_node('movement_test')
     sm = smach.StateMachine(outcomes=['done', 'fail'])
-    sm.userdata.args = {'x' : 1, 'y' : 2}
-    sm.type = 'move'
-
+    sm.userdata.args = {'x' : 2, 'y' : 2, 'angle' : 36.4, 'depth' : 3.14}
     with sm:
+        sm.userdata.type = 'move'
+        smach.StateMachine.add('MOVE', Move(),
+                                transitions={'Success': 'done',
+                                            'Failure': 'fail'},
+                                remapping={'type':'type','args':'args'})
+        sm.userdata.type = 'depth'
+        smach.StateMachine.add('MOVE', Move(),
+                                transitions={'Success': 'done',
+                                            'Failure': 'fail'},
+                                remapping={'type':'type','args':'args'})
+        sm.userdata.type = 'yaw'
+        smach.StateMachine.add('MOVE', Move(),
+                                transitions={'Success': 'done',
+                                            'Failure': 'fail'},
+                                remapping={'type':'type','args':'args'})
+        sm.userdata.type = 'pitch'
+        smach.StateMachine.add('MOVE', Move(),
+                                transitions={'Success': 'done',
+                                            'Failure': 'fail'},
+                                remapping={'type':'type','args':'args'})
+        sm.userdata.type = 'roll'
         smach.StateMachine.add('MOVE', Move(),
                                 transitions={'Success': 'done',
                                             'Failure': 'fail'},
--- states/Move.py
+++ states/Move.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python
+
 import rospy
 import smach
 from actionTools import *
@@ -9,19 +11,27 @@ class Move(smach.State):
             input_keys=['type','args'])
     
     def execute(self, userdata):
-        if type == 'move':
-            rospy.loginfo('Moving by vector <%f, %f>'%(userdata.args.x, userdata.args.y))
-            moveAction(userdata.args.x, userdata.args.x).wait_for_result()
-        elif type == 'depth':
-            rospy.loginfo('Moving to depth %f'%userdata.args.depth)
-            depthAction(userdata.args.depth)
-        elif type == 'yaw':
-            rospy.loginfo('Yawing with angle %f'%userdata.args.angle)
-            yawAction(userdata.args.angle)
-        elif type == 'pitch':
-            rospy.loginfo('Pitching with angle %f'%userdata.args.angle)
-            pitchAction(userdata.args.angle)
-        elif type == 'roll':
-            rospy.loginfo('Rolling with angle %f'%userdata.args.angle)
-            rollAction(userdata.args.angle)
-        return 'Success'
+        status = 'Failure'
+        if userdata.type == 'move':
+            rospy.loginfo('Moving by vector <%f, %f>'%(userdata.args['x'], userdata.args['y']))
+            moveAction(userdata.args['x'], userdata.args['y'])
+            status = 'Success'
+        elif userdata.type == 'depth':
+            rospy.loginfo('Moving to depth %f'%userdata.args['depth'])
+            depthAction(userdata.args['depth'])
+            status = 'Success'
+        elif userdata.type == 'yaw':
+            rospy.loginfo('Yawing with angle %f'%userdata.args['angle'])
+            yawAction(userdata.args['angle'])
+            status = 'Success'
+        elif userdata.type == 'pitch':
+            rospy.loginfo('Pitching with angle %f'%userdata.args['angle'])
+            pitchAction(userdata.args['angle'])
+            status = 'Success'
+        elif userdata.type == 'roll':
+            rospy.loginfo('Rolling with angle %f'%userdata.args['angle'])
+            rollAction(userdata.args['angle'])
+            status = 'Success'
+        else:
+            rospy.loginfo('ERROR: type of %s not recognized'%userdata.type)
+        return status
