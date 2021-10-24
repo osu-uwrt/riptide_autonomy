9df2b7274a2fe6f5d892888c57ddf84d2314ea97
tobymcguire0 <tobymcguire461@gmail.com>
2021-10-24 15:17:32 -0400

    Started search_state with pseudocode

--- src/riptide_autonomy/search_state.cpp
+++ src/riptide_autonomy/search_state.cpp
@@ -13,6 +13,7 @@ using namespace states;
  */
 void search_state::init() {
     positionPublisher = n.advertise<geometry_msgs::Vector3>(positionTopic, CACHE_SIZE);
+    //Subscribe to whatever publishes the guess data
 }
 
 /**
@@ -22,6 +23,7 @@ PortsList search_state::providedPorts() {
     return {
         InputPort<std::string>("frame"),
         InputPort<std::string>("target_error")
+        InputPort<std::string>("update_ms")
     };
 }
 
@@ -33,5 +35,27 @@ PortsList search_state::providedPorts() {
  */
 NodeStatus search_state::tick() {
     ROS_INFO("BIG Search");
+    bool sunnyDay = true;
+    std:string frame = getInput<std:string>("frame").value();
+    double target_error = stod(getInput<std:String>("target_error").value());
+    double update_sec = stod(InputPort<std::string>("update_sec").value());
+    //Set the guess point as the point to move toward
+    
+    //Get a snapshot of the time at the start
+    ros::Time begin = ros::Time::now();
+
+    while(sunnyDay){
+        if(ros:Time::now()-begin >=ros::Duration(update_sec){
+
+            //Get the new point estimate error and location
+            //If the esitmate error < = target_error, return success
+            //Else set new target point to the new location
+
+            //If the distance from the robot to the new location is < 1ft, sunnyDay = false;
+            begin = ros::Time::now();
+        }
+    }
+
+
     return NodeStatus::SUCCESS;
 }
