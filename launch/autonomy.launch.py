e9d1510b14fb1af45b9dcf5ad78c9baf9045fd13
mitchsayre <sketch0306@gmail.com>
2022-09-02 15:05:18 -0400

    fixed autonomy transform

--- launch/autonomy.launch.py
+++ launch/autonomy.launch.py
@@ -1,25 +1,28 @@
 import launch
 import launch.actions
-from ament_index_python.packages import get_package_share_directory
-import launch_ros.actions
-import os
-from launch.actions import DeclareLaunchArgument
+from launch_ros.actions import Node, PushRosNamespace
 
 def generate_launch_description():
     return launch.LaunchDescription([
-        # create the nodes
-        launch_ros.actions.Node(
-            package='tf2_ros',
-            executable='static_transform_publisher',
-            name='torpedo_hole_publisher',
-            arguments=['0', '0.1524', '0.381', '0' '0', '0', 'torpedoGman_frame', 'torpedoHole_frame']
-        ),
-         
-        launch_ros.actions.Node(
-            package='riptide_autonomy2',
-            executable='AlignTorpedos.py',
-            name='AlignTorpedos',
-            respawn=False,
-            output='screen',
+        launch.actions.GroupAction(
+            actions=[
+                PushRosNamespace("/tempest"),
+                # create the nodes
+                Node(
+                    package='tf2_ros',
+                    executable='static_transform_publisher',
+                    name='torpedo_hole_publisher',
+                    arguments=["0", "0.1524", "0.381", "0", "0", "0", "torpedoGman_frame", "torpedoHole_frame"]
+                ),
+                
+                Node(
+                    package='riptide_autonomy2',
+                    executable='AlignTorpedos.py',
+                    name='AlignTorpedos',
+                    respawn=False,
+                    output='screen',
+                )
+            ]
         )
     ])
+
