25c58845d56312aea1e8b356e79e964057b24eeb
Brach Knutson <brach2003@gmail.com>
2023-05-16 00:22:26 -0400

    removed GetActuatorStatus for now. changed to new kill topic

--- test/riptide_autonomy/bt_actions/TestGetBoolTopic.cpp
+++ test/riptide_autonomy/bt_actions/TestGetBoolTopic.cpp
@@ -1,6 +1,6 @@
 #include "autonomy_test/autonomy_testing.hpp"
 
-TEST_F(BtTest, test_GetActuatorStatus) {
+TEST_F(BtTest, test_GetBoolTopic) {
     //TODO: define a test here and delete below line when you do
     GTEST_SKIP();
 }
